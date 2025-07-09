dbb960dd32e30b363607e818cc21a50732f469cb
Xurshiddd <marifovxurshid800@gmail.com>
2025-07-09 20:39:52 +0500

    asdasd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,11 +33,11 @@
     <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded shadow">
         <strong>Success!</strong> {{ session('success') }}
     </div>
-    @elseif (session('error'))
-    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded shadow">
-        <strong>Error!</strong> {{ session('error') }}
+    @elseif ($errors->has('error'))
+    <div class="bg-red-100 text-red-700 px-4 py-2 rounded mt-2 text-center">
+        {{ $errors->first('error') }}
     </div>
-    @endif
+@endif
 </div>
 <!-- Alert -->
 <div class="bg-red-500 text-white text-center py-2 font-medium">
