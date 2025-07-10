e4e277ff16f2c51bb23cad39913aeab0922e4165
Xurshiddd <marifovxurshid800@gmail.com>
2025-07-10 22:03:23 +0500

    90%

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -39,7 +39,7 @@
 <div class="container min-h-screen" >
 
     {{-- Include the sidebar --}}
-    <div class="mx-auto px-4 py-8 overflow-y-scroll max-h-[500px]">
+    <div class="mx-auto px-4 py-8 overflow-y-scroll max-h-[90%]">
         <div class="flex flex-col md:flex-row justify-between items-center mb-8">
             <h1 class="text-3xl font-bold text-indigo-700 mb-4 md:mb-0">
                 <i class="fas fa-user-graduate mr-2"></i>Student Management System
