c4390f7b91315714d287f5a83a2c7061b522709b
Xurshiddd <marifovxurshid800@gmail.com>
2025-07-10 21:56:50 +0500

    pagination

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -36,7 +36,9 @@
         color: white;
     }
 </style>
-<div class="container min-h-screen">
+<div class="container min-h-screen" style="owerflow-y: scroll;">
+
+    {{-- Include the sidebar --}}
     <div class="mx-auto px-4 py-8">
         <div class="flex flex-col md:flex-row justify-between items-center mb-8">
             <h1 class="text-3xl font-bold text-indigo-700 mb-4 md:mb-0">
@@ -117,7 +119,7 @@
         </div>
         
         <div class="flex justify-between items-center mt-4">
-            {{ $students->links('pagination::bootstrap-5') }}
+            {{ $students->links() }}
         </div>
     </div>
 </div>
