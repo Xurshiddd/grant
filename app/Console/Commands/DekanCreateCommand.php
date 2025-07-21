beed01f9d8f72b6c470e0e3456cacccb139a8f4f
Xurshid Marifov <166377877+Xurshiddd@users.noreply.github.com>
2025-07-21 12:37:32 +0500

    Update DekanCreateCommand.php

--- app/Console/Commands/DekanCreateCommand.php
+++ app/Console/Commands/DekanCreateCommand.php
@@ -45,7 +45,7 @@ class DekanCreateCommand extends Command
                     'surname'           => 'Mansurov',
                     'father_name'       => 'Alisherovich',
                     'full_name'         => 'Mansurov Mansur Alisherovich',
-                    'faculty'           => '331-304',
+                    'faculty'           => '331-104',
                 ],
                 [
                     'uuid'              => Str::uuid(),
@@ -55,7 +55,7 @@ class DekanCreateCommand extends Command
                     'surname'           => 'Patxullayev',
                     'father_name'       => 'Ubaydulla o\'gli',
                     'full_name'         => 'Patxullayev Sarvarjon Ubaydulla o\'gli',
-                    'faculty'           => '331-302',
+                    'faculty'           => '331-102',
                 ],
                 [
                     'uuid'              => Str::uuid(),
@@ -65,7 +65,7 @@ class DekanCreateCommand extends Command
                     'surname'           => 'Ruzmetov',
                     'father_name'       => 'Erkinovich',
                     'full_name'         => 'Ruzmetov Mansurbek Erkinovich',
-                    'faculty'           => '331-301',
+                    'faculty'           => '331-101',
                 ],
                 [
                     'uuid'              => Str::uuid(),
@@ -75,7 +75,7 @@ class DekanCreateCommand extends Command
                     'surname'           => 'Toshev',
                     'father_name'       => 'Yusupovich',
                     'full_name'         => 'Toshev Akmal Yusupovich',
-                    'faculty'           => '331-303',
+                    'faculty'           => '331-103',
                 ],
                 
             ];
@@ -92,10 +92,10 @@ class DekanCreateCommand extends Command
         
         // 3) Fakultet → guruhlar map’i
         $data = [
-            '331-301' => ['29-24', '22-24', '22a-24', '3-24', '3a-24', '31-24', '24-24', '24a-24', '2-24', '1-24', 'M22-24', 'M3-24', 'M27-24', 'M1-24', 'M24-24', 'M2-24'],
-            '331-302' => ['4-24','5-24','6-24','7-24','8-24','8r-24','9-24','16-24','16r-24','28-24','M23-24','M9-24','M7-24','M4-24','M5-24','M8-24','M16-24', 'M17-24'],
-            '331-303' => ['10-24', '10r-24', '12-20-24', '18-24', '25-24', '25a-24', '25b-24', '25r-24', '27-24', '30-24', '35-24', 'M25-24', 'M10-11-24', 'M12-20-24', 'M30-24'],
-            '331-304' => ['13-24','13a-24','13b-24','13v-24','13r-24','14-24','14a-24','14b-24','14v-24','14r-24','19-24','19a-24','19b-24','19v-24','19r-24','32-24','33-24','34-24','M13-24','M14-24'],
+            '331-101' => ['29-24', '22-24', '22a-24', '3-24', '3a-24', '31-24', '24-24', '24a-24', '2-24', '1-24', 'M22-24', 'M3-24', 'M27-24', 'M1-24', 'M24-24', 'M2-24'],
+            '331-102' => ['4-24','5-24','6-24','7-24','8-24','8r-24','9-24','16-24','16r-24','28-24','M23-24','M9-24','M7-24','M4-24','M5-24','M8-24','M16-24', 'M17-24'],
+            '331-103' => ['10-24', '10r-24', '12-20-24', '18-24', '25-24', '25a-24', '25b-24', '25r-24', '27-24', '30-24', '35-24', 'M25-24', 'M10-11-24', 'M12-20-24', 'M30-24'],
+            '331-104' => ['13-24','13a-24','13b-24','13v-24','13r-24','14-24','14a-24','14b-24','14v-24','14r-24','19-24','19a-24','19b-24','19v-24','19r-24','32-24','33-24','34-24','M13-24','M14-24'],
         ];
         
         foreach ($data as $faculty => $groups) {
