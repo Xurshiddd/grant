d3922e984e078dffb7b47e7ec531662b2a97d921
Xurshiddd <marifovxurshid800@gmail.com>
2025-07-25 11:17:58 +0500

    excel

--- app/Exports/AllStudentExcel.php
+++ app/Exports/AllStudentExcel.php
@@ -14,15 +14,38 @@ class AllStudentExcel implements FromCollection, WithHeadings
     */
     public function collection()
     {
-        return User::where('type', 'student')
-        ->get(['full_name', 'group_name', 'avg_gpa']); 
+
+        $facultyNames = [
+        '331-101' => 'Sanoat texnologiyalari va mexanika fakulteti',
+        '331-102' => 'To‘qimachilik muhandisligi fakulteti',
+        '331-103' => 'Dizayn va texnologiyalar fakulteti',
+        '331-104' => 'Iqtisodiyot fakulteti',
+    ];
+
+    $students = User::where('type', 'student')
+        ->whereHas('petitions')
+        ->with(['audits']) // audits aloqasini yuklaymiz
+        ->whereIn('faculty', array_keys($facultyNames))
+        ->orderByRaw("FIELD(faculty, '331-101', '331-102', '331-103', '331-104')")
+        ->get();
+
+    // Kerakli formatga o‘zgartiramiz
+    return $students->map(function ($user) use ($facultyNames) {
+        return [
+            'faculty' => $facultyNames[$user->faculty] ?? $user->faculty,
+            'full_name' => $user->full_name,
+            'group_name' => $user->group_name,
+            'new_values' => $user->audits->sum('new_values') ?? 0,
+        ];
+    });
     }
     public function headings(): array
     {
         return [
+            'Fakultet',
             'F.I.O',
             'Guruh nomi',
-            'GPA'
+            'Ball'
         ];
     }
 }
