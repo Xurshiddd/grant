80711a72eb419137c3fe6275f6f97bae26d7e35a
Xurshiddd <marifovxurshid800@gmail.com>
2025-07-29 14:30:10 +0500

    message

--- app/Console/Commands/ScoreUpdate.php
+++ app/Console/Commands/ScoreUpdate.php
@@ -4,52 +4,58 @@ namespace App\Console\Commands;
 
 use App\Models\User;
 use App\Models\Audit;
+use App\Models\Message;
 use Illuminate\Console\Command;
 
 class ScoreUpdate extends Command
 {
     /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
+    * The name and signature of the console command.
+    *
+    * @var string
+    */
     protected $signature = 'app:score-update';
-
+    
     /**
-     * The console command description.
-     *
-     * @var string
-     */
+    * The console command description.
+    *
+    * @var string
+    */
     protected $description = 'Ball qo\'yish';
-
+    
     /**
-     * Execute the console command.
-     */
+    * Execute the console command.
+    */
     public function handle()
     {
         $students = User::where('type', 'student')->get();
-
+        
         foreach ($students as $student) {
             $gpa = round($student->avg_gpa, 1);
             $score = $this->gpaToScore($gpa);
-
+            
             // Audit logga yozish
-            Audit::create([
+            // Audit::create([
+            //     'user_id' => $student->id,
+            //     'event' => 'Baholash',
+            //     'category_id' => 13,
+            //     'comment' => "Talabaning GPA ko‘rsatkichi: $gpa",
+            //     'auditable_id' => 1,
+            //     'old_values' => '0',
+            //     'new_values' => $score,
+            // ]);
+            Message::create([
                 'user_id' => $student->id,
-                'event' => 'Baholash',
-                'category_id' => 13,
-                'comment' => "Talabaning GPA ko‘rsatkichi: $gpa",
-                'auditable_id' => 1,
-                'old_values' => '0',
-                'new_values' => $score,
+                'subject' => 'Baholash',
+                'body' => "Sizga 'Talabaning akademik oʻzlashtirishi' mezoni bo'yicha {$score} ball berildi.",
+                'is_read' => false,
             ]);
-
             $this->info("{$student->full_name} - GPA: $gpa, Score: $score");
         }
-
+        
         return Command::SUCCESS;
     }
-     private function gpaToScore($gpa)
+    private function gpaToScore($gpa)
     {
         $map = [
             5.0 => 10.0,
@@ -69,7 +75,7 @@ class ScoreUpdate extends Command
             3.6 => 5.3,
             3.5 => 5.0,
         ];
-
+        
         return $map[$gpa] ?? 0;
     }
 }
