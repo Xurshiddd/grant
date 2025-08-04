df05c8a72e6599946b4638ddf3c54c957e5f2e23
Xurshiddd <marifovxurshid800@gmail.com>
2025-08-04 18:13:37 +0500

    aaa

--- app/Models/StudentData.php
+++ app/Models/StudentData.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class StudentData extends Model
 {
-    protected $table = 'student_data';
     protected $fillable = ['user_id', 'data'];
     public function user(): BelongsTo{
         return $this->belongsTo(User::class);
